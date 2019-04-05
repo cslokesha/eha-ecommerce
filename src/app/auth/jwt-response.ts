055fdc4ec55f719d0055bcd62f912e20a989f210
unknown <dkr1991@outlook.com>
2019-04-05 19:55:06 +0530

    removed spec and auth

--- src/app/auth/jwt-response.ts
+++
@@ -1,8 +0,0 @@
-import { Response } from '@angular/http';
-export class JwtResponse {
-    accessToken: string;
-    type: string;
-    username: string;
-    authorities: string[];
-    Response:string[];
-}
--- src/app/auth/login-info.ts
+++
@@ -1,9 +0,0 @@
-export class AuthLoginInfo {
-    email: string;
-    password: string;
-
-    constructor(username: string, password: string) {
-        this.email = username;
-        this.password = password;
-    }
-}
